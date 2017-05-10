napo/longomatch-core

New PlayEditorController to handle logic in PlayEditor

LongoMatch.Services/ViewModel/PlayEditorVM.cs
@@ -16,11 +16,12 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 //
 using System;
-using System.Linq;
+using System.Threading.Tasks;
 using LongoMatch.Core.Store;
 using LongoMatch.Core.ViewModel;
 using LongoMatch.Services.Interfaces;
 using VAS.Core.Common;
+using VAS.Core.Events;
 using VAS.Core.MVVMC;
 
 namespace LongoMatch.Services.ViewModel
@@ -68,11 +69,12 @@ namespace LongoMatch.Services.ViewModel
 		/// <value>The team tagger.</value>
 		public LMTeamTaggerVM TeamTagger {
 			get;
+			protected set;
 		}
 
 		public LMProjectVM Project {
 			get {
-				throw new NotImplementedException ();
+				return this;
 			}
 		}
 
@@ -84,8 +86,8 @@ namespace LongoMatch.Services.ViewModel
 
 		void ResetTeamTagger ()
 		{
-			TeamTagger.AwayTeam.Model = Model.VisitorTeamTemplate;
-			TeamTagger.HomeTeam.Model = Model.LocalTeamTemplate;
+			TeamTagger.AwayTeam = AwayTeam;
+			TeamTagger.HomeTeam = HomeTeam;
 			TeamTagger.Background = Model.Dashboard?.FieldBackground;
 			UpdateTeamTagger ();
 		}
@@ -96,13 +98,23 @@ namespace LongoMatch.Services.ViewModel
 				foreach (var player in TeamTagger.HomeTeam) {
 					if (play.Players.Contains (player.Model)) {
 						player.Tagged = true;
+						TeamTagger.HomeTeam.Selection.Add (player);
 					}
 				}
 				foreach (var player in TeamTagger.AwayTeam) {
 					if (play.Players.Contains (player.Model)) {
 						player.Tagged = true;
+						TeamTagger.AwayTeam.Selection.Add (player);
 					}
 				}
+				foreach (var team in play.Teams) {
+					if (team == TeamTagger.HomeTeam.Model) {
+						TeamTagger.HomeTeam.Tagged = true;
+					} else if (team == TeamTagger.AwayTeam.Model) {
+						TeamTagger.AwayTeam.Tagged = true;
+					}
+				}
+				TeamTagger.CurrentTime = play.EventTime;
 			}
 		}
 	}

LongoMatch/longomatch

LongoMatch.Services/ViewModel/PlayEditorVM.cs
@@ -16,17 +16,19 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 //
 using System;
-using System.Linq;
+using System.Threading.Tasks;
 using LongoMatch.Core.Store;
 using LongoMatch.Core.ViewModel;
 using LongoMatch.Services.Interfaces;
 using VAS.Core.Common;
+using VAS.Core.Events;
 using VAS.Core.MVVMC;
 
 namespace LongoMatch.Services.ViewModel
 {
 	/// <summary>
-	/// Play Editor view model
+	/// Play Editor view model, used in PlayEditor View. It needs a TimelineEvent.
+	/// It has a LMTeamTaggerVM for the teamtagger component that auto initializes.
 	/// </summary>
 	public class PlayEditorVM : LMProjectVM, ILMTeamTaggerVM, ILMProjectVM
 	{
@@ -68,11 +70,12 @@ namespace LongoMatch.Services.ViewModel
 		/// <value>The team tagger.</value>
 		public LMTeamTaggerVM TeamTagger {
 			get;
+			protected set;
 		}
 
 		public LMProjectVM Project {
 			get {
-				throw new NotImplementedException ();
+				return this;
 			}
 		}
 
@@ -84,26 +87,37 @@ namespace LongoMatch.Services.ViewModel
 
 		void ResetTeamTagger ()
 		{
-			TeamTagger.AwayTeam.Model = Model.VisitorTeamTemplate;
-			TeamTagger.HomeTeam.Model = Model.LocalTeamTemplate;
+			TeamTagger.AwayTeam = AwayTeam;
+			TeamTagger.HomeTeam = HomeTeam;
 			TeamTagger.Background = Model.Dashboard?.FieldBackground;
 			UpdateTeamTagger ();
 		}
 
 		void UpdateTeamTagger ()
 		{
-			if (play != null) {
-				foreach (var player in TeamTagger.HomeTeam) {
-					if (play.Players.Contains (player.Model)) {
-						player.Tagged = true;
-					}
-				}
-				foreach (var player in TeamTagger.AwayTeam) {
-					if (play.Players.Contains (player.Model)) {
-						player.Tagged = true;
-					}
+			if (Play == null) {
+				return;
+			}
+			foreach (var player in TeamTagger.HomeTeam) {
+				if (Play.Players.Contains (player.Model)) {
+					player.Tagged = true;
+					TeamTagger.HomeTeam.Selection.Add (player);
 				}
 			}
+			foreach (var player in TeamTagger.AwayTeam) {
+				if (Play.Players.Contains (player.Model)) {
+					player.Tagged = true;
+					TeamTagger.AwayTeam.Selection.Add (player);
+				}
+			}
+			foreach (var team in Play.Teams) {
+				if (team == TeamTagger.HomeTeam.Model) {
+					TeamTagger.HomeTeam.Tagged = true;
+				} else if (team == TeamTagger.AwayTeam.Model) {
+					TeamTagger.AwayTeam.Tagged = true;
+				}
+			}
+			TeamTagger.CurrentTime = Play.EventTime;
 		}
 	}
 }