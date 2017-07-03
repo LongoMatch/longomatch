napo/longomatch-core

Start using the new ImageView and CellRendererImage

LongoMatch.GUI/Gui/Component/DashboardWidget.cs
@@ -60,7 +60,7 @@ namespace LongoMatch.Gui.Component
 		{
 			this.Build ();
 
-			applyimage.Pixbuf = Helpers.Misc.LoadIcon ("longomatch-apply", IconSize.Button);
+			applyimage.Image = App.Current.ResourcesLocator.LoadIcon ("longomatch-apply", StyleConf.ButtonDialogIconSize);
 
 			tagger = new DashboardCanvas (new WidgetWrapper (drawingarea));
 			tagger.ShowMenuEvent += HandleShowMenuEvent;

LongoMatch.GUI/Gui/Component/GameDescriptionHeader.cs
@@ -17,6 +17,7 @@
 //
 using Gtk;
 using LongoMatch.Core.Store;
+using VAS.Core;
 using Misc = VAS.UI.Helpers.Misc;
 
 namespace LongoMatch.Gui.Component
@@ -36,22 +37,24 @@ namespace LongoMatch.Gui.Component
 			scoreGroup = new SizeGroup (SizeGroupMode.Horizontal);
 			scoreGroup.AddWidget (homescorelabel);
 			scoreGroup.AddWidget (awayscorelabel);
+			homeimage.SetSize (50, 50);
+			awayimage.SetSize (50, 50);
 		}
 
 		public ProjectDescription ProjectDescription {
 			set {
 				if (value.LocalShield != null) {
-					homeimage.Pixbuf = value.LocalShield.Scale (100, 50).Value;
+					homeimage.Image = value.LocalShield;
 				} else {
-					homeimage.Pixbuf = Misc.LoadIcon ("longomatch-default-shield", 50);
+					homeimage.Image = App.Current.ResourcesLocator.LoadIcon ("longomatch-default-shield", 50);
 				}
 				homenamelabel.Text = value.LocalName;
 				homescorelabel.Text = value.LocalGoals.ToString ();
-				
+
 				if (value.VisitorShield != null) {
-					awayimage.Pixbuf = value.VisitorShield.Scale (100, 50).Value;
+					awayimage.Image = value.VisitorShield;
 				} else {
-					awayimage.Pixbuf = Misc.LoadIcon ("longomatch-default-shield", 50);
+					awayimage.Image = App.Current.ResourcesLocator.LoadIcon ("longomatch-default-shield", 50);
 				}
 				awaynamelabel.Text = value.VisitorName;
 				awayscorelabel.Text = value.VisitorGoals.ToString ();

LongoMatch.GUI/Gui/Component/TeamTemplateEditor.cs
@@ -16,12 +16,9 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 //
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using Gdk;
 using Gtk;
-using LongoMatch.Core.Common;
 using LongoMatch.Core.Store;
 using LongoMatch.Core.Store.Templates;
 using LongoMatch.Drawing.Widgets;
@@ -30,7 +27,6 @@ using VAS.Core;
 using VAS.Core.Common;
 using VAS.Core.Events;
 using VAS.Core.Interfaces.MVVMC;
-using VAS.Core.Store;
 using VAS.Drawing.Cairo;
 using VAS.UI.UI.Component;
 using Color = VAS.Core.Common.Color;
@@ -59,8 +55,12 @@ namespace LongoMatch.Gui.Component
 		{
 			this.Build ();
 
+			applyimage.Image = App.Current.ResourcesLocator.LoadIcon ("longomatch-apply", StyleConf.ButtonDialogIconSize);
+
 			teamtagger = new LMTeamTaggerView (new WidgetWrapper (drawingarea));
-			shieldimage.HeightRequest = shieldvbox.WidthRequest = SHIELD_SIZE;
+			shieldvbox.WidthRequest = SHIELD_SIZE;
+			shieldimage.SetSize (SHIELD_SIZE, SHIELD_SIZE);
+			playerimage.SetSize (PLAYER_SIZE, PLAYER_SIZE);
 			colorbutton1.Color = Misc.ToGdkColor (Color.Red1);
 			colorbutton1.ColorSet += HandleColorSet;
 			colorbutton2.Color = Misc.ToGdkColor (Color.Green1);
@@ -92,9 +92,9 @@ namespace LongoMatch.Gui.Component
 				template = value;
 				ignoreChanges = true;
 				if (template.Shield != null) {
-					shieldimage.Pixbuf = template.Shield.Scale (SHIELD_SIZE, SHIELD_SIZE).Value;
+					shieldimage.Image = template.Shield;
 				} else {
-					shieldimage.Pixbuf = Helpers.Misc.LoadIcon ("longomatch-default-shield", SHIELD_SIZE);
+					shieldimage.Image = App.Current.ResourcesLocator.LoadIcon ("longomatch-default-shield", SHIELD_SIZE);
 				}
 				teamnameentry.Text = template.TeamName;
 				FillFormation ();
@@ -232,7 +232,7 @@ namespace LongoMatch.Gui.Component
 			nationalityentry.Text = p.Nationality ?? "";
 			bdaydatepicker.Date = p.Birthday;
 			mailentry.Text = p.Mail ?? "";
-			playerimage.Pixbuf = PlayerPhoto (p);
+			playerimage.Image = PlayerPhoto (p);
 
 			playerframe.Sensitive = true;
 
@@ -257,7 +257,7 @@ namespace LongoMatch.Gui.Component
 			nationalityentry.Text = "";
 			bdaydatepicker.Date = new DateTime ();
 			mailentry.Text = "";
-			playerimage.Pixbuf = playerimage.Pixbuf = Helpers.Misc.LoadIcon ("longomatch-player-pic", PLAYER_SIZE, IconLookupFlags.ForceSvg);
+			playerimage.Image = App.Current.ResourcesLocator.LoadIcon ("longomatch-player-pic", PLAYER_SIZE);
 
 			ignoreChanges = false;
 		}
@@ -274,14 +274,14 @@ namespace LongoMatch.Gui.Component
 			FillFormation ();
 		}
 
-		Pixbuf PlayerPhoto (LMPlayer p)
+		Image PlayerPhoto (LMPlayer p)
 		{
-			Pixbuf playerImage;
+			Image playerImage;
 
 			if (p.Photo != null) {
-				playerImage = p.Photo.Scale (PLAYER_SIZE, PLAYER_SIZE).Value;
+				playerImage = p.Photo;
 			} else {
-				playerImage = Misc.LoadIcon ("longomatch-player-pic", PLAYER_SIZE, IconLookupFlags.ForceSvg);
+				playerImage = App.Current.ResourcesLocator.LoadIcon ("longomatch-player-pic", PLAYER_SIZE);
 			}
 			return playerImage;
 		}
@@ -321,38 +321,32 @@ namespace LongoMatch.Gui.Component
 
 		void HandlePlayerButtonPressEvent (object o, ButtonPressEventArgs args)
 		{
-			Image player;
-			Pixbuf pix = Helpers.Misc.OpenImage (this);
+			Image newPlayerImage = Helpers.Misc.OpenImage (this);
 
-			if (pix == null) {
+			if (newPlayerImage == null) {
 				return;
 			}
 
-			player = new Image (pix);
-			player.ScaleInplace (Constants.MAX_PLAYER_ICON_SIZE,
-				Constants.MAX_PLAYER_ICON_SIZE);
-			if (player != null && loadedPlayer != null) {
-				playerimage.Pixbuf = player.Scale (PLAYER_SIZE, PLAYER_SIZE).Value;
-				loadedPlayer.Photo = player;
+			newPlayerImage.ScaleInplace (Constants.MAX_PLAYER_ICON_SIZE, Constants.MAX_PLAYER_ICON_SIZE);
+			if (newPlayerImage != null && loadedPlayer != null) {
+				playerimage.Image = newPlayerImage;
+				loadedPlayer.Photo = newPlayerImage;
 				Edited = true;
 			}
 		}
 
 		void HandleShieldButtonPressEvent (object o, ButtonPressEventArgs args)
 		{
-			Image shield;
-			Pixbuf pix = Helpers.Misc.OpenImage (this);
+			Image shield = Helpers.Misc.OpenImage (this);
 
-			if (pix == null) {
+			if (shield == null) {
 				return;
 			}
 
-			shield = new Image (pix);
 			if (shield != null) {
-				shield.ScaleInplace (Constants.MAX_SHIELD_ICON_SIZE,
-					Constants.MAX_SHIELD_ICON_SIZE);
+				shield.ScaleInplace (Constants.MAX_SHIELD_ICON_SIZE, Constants.MAX_SHIELD_ICON_SIZE);
 				template.Shield = shield;
-				shieldimage.Pixbuf = shield.Scale (SHIELD_SIZE, SHIELD_SIZE).Value;
+				shieldimage.Image = shield;
 				Edited = true;
 			}
 		}

LongoMatch.GUI/Gui/Component/TeamsComboBox.cs
@@ -21,51 +21,51 @@ using Gtk;
 using LongoMatch.Core.Store.Templates;
 using VAS.Core.Common;
 using Helpers = VAS.UI.Helpers;
+using Image = VAS.Core.Common.Image;
+using VAS.Core;
+using VAS.UI.Component;
 
 namespace LongoMatch.Gui.Component
 {
 	[System.ComponentModel.Category ("LongoMatch")]
 	[System.ComponentModel.ToolboxItem (true)]
-	public class TeamsComboBox: Gtk.ComboBox
+	public class TeamsComboBox : Gtk.ComboBox
 	{
 		ListStore store;
-		CellRendererPixbuf pixrender;
+		CellRendererImage imageRenderer;
 		CellRendererText texrender;
-
-		public TeamsComboBox ()
-		{
-		}
 
 		public void Load (List<LMTeam> teams)
 		{
 			Clear ();
-			pixrender = new CellRendererPixbuf ();
+			imageRenderer = new CellRendererImage ();
+			imageRenderer.Width = StyleConf.NewTeamsIconSize;
+			imageRenderer.Height = StyleConf.NewTeamsIconSize;
 			texrender = new CellRendererText ();
 			texrender.Font = App.Current.Style.Font + " " + StyleConf.NewTeamsFontSize;
 			texrender.Alignment = Pango.Alignment.Center;
 
 			if (Direction == TextDirection.Ltr) {
-				PackStart (pixrender, false);
+				PackStart (imageRenderer, false);
 				PackEnd (texrender, true);
 			} else {
-				PackEnd (pixrender, false);
+				PackEnd (imageRenderer, false);
 				PackStart (texrender, true);
 			}
-			
-			store = new ListStore (typeof(Pixbuf), typeof(string), typeof(LMTeam));
+
+			store = new ListStore (typeof (Image), typeof (string), typeof (LMTeam));
 			foreach (LMTeam t in teams) {
-				Pixbuf shield;
-				int size = StyleConf.NewTeamsIconSize;
+				Image shield;
 
 				if (t.Shield == null) {
-					shield = Helpers.Misc.LoadIcon ("longomatch-default-shield", size);
+					shield = App.Current.ResourcesLocator.LoadIcon ("longomatch-default-shield", StyleConf.NewTeamsIconSize);
 				} else {
-					shield = t.Shield.Scale (size, size).Value;
+					shield = t.Shield;
 				}
 				store.AppendValues (shield, t.Name, t);
 			}
 			SetAttributes (texrender, "text", 1);
-			SetAttributes (pixrender, "pixbuf", 0);
+			SetAttributes (imageRenderer, "Image", 0);
 			Model = store;
 		}
 
@@ -81,7 +81,7 @@ namespace LongoMatch.Gui.Component
 
 	[System.ComponentModel.Category ("LongoMatch")]
 	[System.ComponentModel.ToolboxItem (true)]
-	public class HomeTeamsComboBox: TeamsComboBox
+	public class HomeTeamsComboBox : TeamsComboBox
 	{
 		public HomeTeamsComboBox ()
 		{
@@ -91,7 +91,7 @@ namespace LongoMatch.Gui.Component
 
 	[System.ComponentModel.Category ("LongoMatch")]
 	[System.ComponentModel.ToolboxItem (true)]
-	public class AwayTeamsComboBox: TeamsComboBox
+	public class AwayTeamsComboBox : TeamsComboBox
 	{
 		public AwayTeamsComboBox ()
 		{

LongoMatch.GUI/Gui/Dialog/CodecsChoiceDialog.cs
@@ -29,8 +29,7 @@ namespace LongoMatch.Gui.Dialog
 		{
 			this.Build ();
 
-			Image img = App.Current.ResourcesLocator.LoadImage ("images/longomatch-pro-small.png");
-			buttonOKimage.Pixbuf = img.Value;
+			buttonOKimage.Image = App.Current.ResourcesLocator.LoadImage ("images/longomatch-pro-small.png");
 
 			titlelabel.ModifyFont (FontDescription.FromString (App.Current.Style.Font + " 14"));
 

LongoMatch.GUI/Gui/Panel/TeamsTemplatesPanel.cs
@@ -30,7 +30,9 @@ using VAS.Core.Hotkeys;
 using VAS.Core.Interfaces.GUI;
 using VAS.Core.MVVMC;
 using VAS.Core.ViewModel;
+using VAS.UI.Component;
 using VAS.UI.Helpers.Bindings;
+using Image = VAS.Core.Common.Image;
 using Helpers = VAS.UI.Helpers;
 
 namespace LongoMatch.Gui.Panel
@@ -54,9 +56,11 @@ namespace LongoMatch.Gui.Panel
 			panelheader1.Title = Title;
 			panelheader1.BackClicked += (sender, e) => App.Current.StateController.MoveBack ();
 
-			teamimage.Pixbuf = Helpers.Misc.LoadIcon ("longomatch-team-header", StyleConf.TemplatesHeaderIconSize);
-			playerheaderimage.Pixbuf = Helpers.Misc.LoadIcon ("longomatch-player-header", StyleConf.TemplatesHeaderIconSize);
-			vseparatorimage.Pixbuf = Helpers.Misc.LoadIcon ("vertical-separator", StyleConf.TemplatesIconSize);
+			teamimage.Image = App.Current.ResourcesLocator.LoadIcon ("longomatch-team-header", StyleConf.TemplatesHeaderIconSize);
+			playerheaderimage.Image = App.Current.ResourcesLocator.LoadIcon ("longomatch-player-header", StyleConf.TemplatesHeaderIconSize);
+			// FIXME: "vertical-separator" has a png extension
+			vseparatorimage.Image = new Image (Helpers.Misc.LoadIcon ("vertical-separator", StyleConf.TemplatesIconSize));
+
 
 			newteambutton.Entered += HandleEnterTeamButton;
 			newteambutton.Left += HandleLeftTeamButton;
@@ -82,7 +86,7 @@ namespace LongoMatch.Gui.Panel
 			cell.Edited += HandleEdited;
 			teamseditortreeview.Model = teamsStore;
 			teamseditortreeview.HeadersVisible = false;
-			teamseditortreeview.AppendColumn ("Icon", new CellRendererPixbuf (), RenderIcon);
+			teamseditortreeview.AppendColumn ("Icon", new CellRendererImage (), RenderIcon);
 			var col = teamseditortreeview.AppendColumn ("Text", cell, RenderTemplateName);
 			col.AddAttribute (cell, "editable", COL_EDITABLE);
 			teamseditortreeview.SearchColumn = COL_TEAM;
@@ -185,7 +189,10 @@ namespace LongoMatch.Gui.Panel
 		void RenderIcon (TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter)
 		{
 			LMTeamVM teamVM = (LMTeamVM)model.GetValue (iter, COL_TEAM);
-			(cell as CellRendererPixbuf).Pixbuf = teamVM.Icon.Scale (SHIELD_SIZE, SHIELD_SIZE).Value;
+			CellRendererImage renderer = (cell as CellRendererImage);
+			renderer.Width = SHIELD_SIZE;
+			renderer.Height = SHIELD_SIZE;
+			renderer.Image = teamVM.Icon;
 		}
 
 		void RenderTemplateName (TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter)

LongoMatch/longomatch

LongoMatch.GUI/Gui/Component/DashboardWidget.cs
@@ -60,7 +60,7 @@ namespace LongoMatch.Gui.Component
 		{
 			this.Build ();
 
-			applyimage.Pixbuf = Helpers.Misc.LoadIcon ("vas-apply", IconSize.Button);
+			applyimage.Image = App.Current.ResourcesLocator.LoadIcon ("vas-apply", StyleConf.ButtonDialogIconSize);
 
 			tagger = new DashboardCanvas (new WidgetWrapper (drawingarea));
 			tagger.ShowMenuEvent += HandleShowMenuEvent;

LongoMatch.GUI/Gui/Component/GameDescriptionHeader.cs
@@ -17,6 +17,7 @@
 //
 using Gtk;
 using LongoMatch.Core.Store;
+using VAS.Core;
 using Misc = VAS.UI.Helpers.Misc;
 
 namespace LongoMatch.Gui.Component
@@ -36,22 +37,24 @@ namespace LongoMatch.Gui.Component
 			scoreGroup = new SizeGroup (SizeGroupMode.Horizontal);
 			scoreGroup.AddWidget (homescorelabel);
 			scoreGroup.AddWidget (awayscorelabel);
+			homeimage.SetSize (100, 50);
+			awayimage.SetSize (100, 50);
 		}
 
 		public ProjectDescription ProjectDescription {
 			set {
 				if (value.LocalShield != null) {
-					homeimage.Pixbuf = value.LocalShield.Scale (100, 50).Value;
+					homeimage.Image = value.LocalShield;
 				} else {
-					homeimage.Pixbuf = Misc.LoadIcon ("vas-default-shield", 50);
+					homeimage.Image = App.Current.ResourcesLocator.LoadIcon ("vas-default-shield", 50);
 				}
 				homenamelabel.Text = value.LocalName;
 				homescorelabel.Text = value.LocalGoals.ToString ();
-				
+
 				if (value.VisitorShield != null) {
-					awayimage.Pixbuf = value.VisitorShield.Scale (100, 50).Value;
+					awayimage.Image = value.VisitorShield;
 				} else {
-					awayimage.Pixbuf = Misc.LoadIcon ("vas-default-shield", 50);
+					awayimage.Image = App.Current.ResourcesLocator.LoadIcon ("vas-default-shield", 50);
 				}
 				awaynamelabel.Text = value.VisitorName;
 				awayscorelabel.Text = value.VisitorGoals.ToString ();

LongoMatch.GUI/Gui/Component/TeamTemplateEditor.cs
@@ -16,12 +16,9 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 //
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using Gdk;
 using Gtk;
-using LongoMatch.Core.Common;
 using LongoMatch.Core.Store;
 using LongoMatch.Core.Store.Templates;
 using LongoMatch.Drawing.Widgets;
@@ -30,7 +27,6 @@ using VAS.Core;
 using VAS.Core.Common;
 using VAS.Core.Events;
 using VAS.Core.Interfaces.MVVMC;
-using VAS.Core.Store;
 using VAS.Drawing.Cairo;
 using VAS.UI.UI.Component;
 using Color = VAS.Core.Common.Color;
@@ -59,8 +55,12 @@ namespace LongoMatch.Gui.Component
 		{
 			this.Build ();
 
+			applyimage.Image = App.Current.ResourcesLocator.LoadIcon ("lm-apply", StyleConf.ButtonDialogIconSize);
+
 			teamtagger = new LMTeamTaggerView (new WidgetWrapper (drawingarea));
-			shieldimage.HeightRequest = shieldvbox.WidthRequest = SHIELD_SIZE;
+			shieldvbox.WidthRequest = SHIELD_SIZE;
+			shieldimage.SetSize (SHIELD_SIZE);
+			playerimage.SetSize (PLAYER_SIZE);
 			colorbutton1.Color = Misc.ToGdkColor (Color.Red1);
 			colorbutton1.ColorSet += HandleColorSet;
 			colorbutton2.Color = Misc.ToGdkColor (Color.Green1);
@@ -92,9 +92,9 @@ namespace LongoMatch.Gui.Component
 				template = value;
 				ignoreChanges = true;
 				if (template.Shield != null) {
-					shieldimage.Pixbuf = template.Shield.Scale (SHIELD_SIZE, SHIELD_SIZE).Value;
+					shieldimage.Image = template.Shield;
 				} else {
-					shieldimage.Pixbuf = Helpers.Misc.LoadIcon ("vas-default-shield", SHIELD_SIZE);
+					shieldimage.Image = App.Current.ResourcesLocator.LoadIcon ("vas-default-shield", SHIELD_SIZE);
 				}
 				teamnameentry.Text = template.TeamName;
 				FillFormation ();
@@ -232,7 +232,7 @@ namespace LongoMatch.Gui.Component
 			nationalityentry.Text = p.Nationality ?? "";
 			bdaydatepicker.Date = p.Birthday;
 			mailentry.Text = p.Mail ?? "";
-			playerimage.Pixbuf = PlayerPhoto (p);
+			playerimage.Image = PlayerPhoto (p);
 
 			playerframe.Sensitive = true;
 
@@ -257,7 +257,7 @@ namespace LongoMatch.Gui.Component
 			nationalityentry.Text = "";
 			bdaydatepicker.Date = new DateTime ();
 			mailentry.Text = "";
-			playerimage.Pixbuf = playerimage.Pixbuf = Helpers.Misc.LoadIcon ("lm-player-pic", PLAYER_SIZE, IconLookupFlags.ForceSvg);
+			playerimage.Image = App.Current.ResourcesLocator.LoadIcon ("lm-player-pic", PLAYER_SIZE);
 
 			ignoreChanges = false;
 		}
@@ -274,14 +274,14 @@ namespace LongoMatch.Gui.Component
 			FillFormation ();
 		}
 
-		Pixbuf PlayerPhoto (LMPlayer p)
+		Image PlayerPhoto (LMPlayer p)
 		{
-			Pixbuf playerImage;
+			Image playerImage;
 
 			if (p.Photo != null) {
-				playerImage = p.Photo.Scale (PLAYER_SIZE, PLAYER_SIZE).Value;
+				playerImage = p.Photo;
 			} else {
-				playerImage = Misc.LoadIcon ("lm-player-pic", PLAYER_SIZE, IconLookupFlags.ForceSvg);
+				playerImage = App.Current.ResourcesLocator.LoadIcon ("lm-player-pic", PLAYER_SIZE);
 			}
 			return playerImage;
 		}
@@ -321,38 +321,32 @@ namespace LongoMatch.Gui.Component
 
 		void HandlePlayerButtonPressEvent (object o, ButtonPressEventArgs args)
 		{
-			Image player;
-			Pixbuf pix = Helpers.Misc.OpenImage (this);
+			Image newPlayerImage = Helpers.Misc.OpenImage (this);
 
-			if (pix == null) {
+			if (newPlayerImage == null) {
 				return;
 			}
 
-			player = new Image (pix);
-			player.ScaleInplace (Constants.MAX_PLAYER_ICON_SIZE,
-				Constants.MAX_PLAYER_ICON_SIZE);
-			if (player != null && loadedPlayer != null) {
-				playerimage.Pixbuf = player.Scale (PLAYER_SIZE, PLAYER_SIZE).Value;
-				loadedPlayer.Photo = player;
+			newPlayerImage.ScaleInplace (Constants.MAX_PLAYER_ICON_SIZE, Constants.MAX_PLAYER_ICON_SIZE);
+			if (newPlayerImage != null && loadedPlayer != null) {
+				playerimage.Image = newPlayerImage;
+				loadedPlayer.Photo = newPlayerImage;
 				Edited = true;
 			}
 		}
 
 		void HandleShieldButtonPressEvent (object o, ButtonPressEventArgs args)
 		{
-			Image shield;
-			Pixbuf pix = Helpers.Misc.OpenImage (this);
+			Image shield = Helpers.Misc.OpenImage (this);
 
-			if (pix == null) {
+			if (shield == null) {
 				return;
 			}
 
-			shield = new Image (pix);
 			if (shield != null) {
-				shield.ScaleInplace (Constants.MAX_SHIELD_ICON_SIZE,
-					Constants.MAX_SHIELD_ICON_SIZE);
+				shield.ScaleInplace (Constants.MAX_SHIELD_ICON_SIZE, Constants.MAX_SHIELD_ICON_SIZE);
 				template.Shield = shield;
-				shieldimage.Pixbuf = shield.Scale (SHIELD_SIZE, SHIELD_SIZE).Value;
+				shieldimage.Image = shield;
 				Edited = true;
 			}
 		}

LongoMatch.GUI/Gui/Component/TeamsComboBox.cs
@@ -21,51 +21,51 @@ using Gtk;
 using LongoMatch.Core.Store.Templates;
 using VAS.Core.Common;
 using Helpers = VAS.UI.Helpers;
+using Image = VAS.Core.Common.Image;
+using VAS.Core;
+using VAS.UI.Component;
 
 namespace LongoMatch.Gui.Component
 {
 	[System.ComponentModel.Category ("LongoMatch")]
 	[System.ComponentModel.ToolboxItem (true)]
-	public class TeamsComboBox: Gtk.ComboBox
+	public class TeamsComboBox : Gtk.ComboBox
 	{
 		ListStore store;
-		CellRendererPixbuf pixrender;
+		CellRendererImage imageRenderer;
 		CellRendererText texrender;
-
-		public TeamsComboBox ()
-		{
-		}
 
 		public void Load (List<LMTeam> teams)
 		{
 			Clear ();
-			pixrender = new CellRendererPixbuf ();
+			imageRenderer = new CellRendererImage ();
+			imageRenderer.Width = StyleConf.NewTeamsIconSize;
+			imageRenderer.Height = StyleConf.NewTeamsIconSize;
 			texrender = new CellRendererText ();
 			texrender.Font = App.Current.Style.Font + " " + StyleConf.NewTeamsFontSize;
 			texrender.Alignment = Pango.Alignment.Center;
 
 			if (Direction == TextDirection.Ltr) {
-				PackStart (pixrender, false);
+				PackStart (imageRenderer, false);
 				PackEnd (texrender, true);
 			} else {
-				PackEnd (pixrender, false);
+				PackEnd (imageRenderer, false);
 				PackStart (texrender, true);
 			}
-			
-			store = new ListStore (typeof(Pixbuf), typeof(string), typeof(LMTeam));
+
+			store = new ListStore (typeof (Image), typeof (string), typeof (LMTeam));
 			foreach (LMTeam t in teams) {
-				Pixbuf shield;
-				int size = StyleConf.NewTeamsIconSize;
+				Image shield;
 
 				if (t.Shield == null) {
-					shield = Helpers.Misc.LoadIcon ("vas-default-shield", size);
+					shield = App.Current.ResourcesLocator.LoadIcon ("vas-default-shield", StyleConf.NewTeamsIconSize);
 				} else {
-					shield = t.Shield.Scale (size, size).Value;
+					shield = t.Shield;
 				}
 				store.AppendValues (shield, t.Name, t);
 			}
 			SetAttributes (texrender, "text", 1);
-			SetAttributes (pixrender, "pixbuf", 0);
+			SetAttributes (imageRenderer, "Image", 0);
 			Model = store;
 		}
 
@@ -81,7 +81,7 @@ namespace LongoMatch.Gui.Component
 
 	[System.ComponentModel.Category ("LongoMatch")]
 	[System.ComponentModel.ToolboxItem (true)]
-	public class HomeTeamsComboBox: TeamsComboBox
+	public class HomeTeamsComboBox : TeamsComboBox
 	{
 		public HomeTeamsComboBox ()
 		{
@@ -91,7 +91,7 @@ namespace LongoMatch.Gui.Component
 
 	[System.ComponentModel.Category ("LongoMatch")]
 	[System.ComponentModel.ToolboxItem (true)]
-	public class AwayTeamsComboBox: TeamsComboBox
+	public class AwayTeamsComboBox : TeamsComboBox
 	{
 		public AwayTeamsComboBox ()
 		{

LongoMatch.GUI/Gui/Dialog/CodecsChoiceDialog.cs
@@ -29,8 +29,7 @@ namespace LongoMatch.Gui.Dialog
 		{
 			this.Build ();
 
-			Image img = App.Current.ResourcesLocator.LoadImage ("images/lm-pro-small.png");
-			buttonOKimage.Pixbuf = img.Value;
+			buttonOKimage.Image = App.Current.ResourcesLocator.LoadImage ("images/lm-pro-small.png");
 
 			titlelabel.ModifyFont (FontDescription.FromString (App.Current.Style.Font + " 14"));
 

LongoMatch.GUI/Gui/Panel/TeamsTemplatesPanel.cs
@@ -19,7 +19,6 @@ using System;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using Gtk;
-using LongoMatch.Core.Store.Templates;
 using LongoMatch.Core.ViewModel;
 using LongoMatch.Services.States;
 using LongoMatch.Services.ViewModel;
@@ -30,8 +29,10 @@ using VAS.Core.Hotkeys;
 using VAS.Core.Interfaces.GUI;
 using VAS.Core.MVVMC;
 using VAS.Core.ViewModel;
+using VAS.UI.Component;
 using VAS.UI.Helpers.Bindings;
 using Helpers = VAS.UI.Helpers;
+using Image = VAS.Core.Common.Image;
 
 namespace LongoMatch.Gui.Panel
 {
@@ -54,9 +55,11 @@ namespace LongoMatch.Gui.Panel
 			panelheader1.Title = Title;
 			panelheader1.BackClicked += (sender, e) => App.Current.StateController.MoveBack ();
 
-			teamimage.Pixbuf = Helpers.Misc.LoadIcon ("lm-team-header", StyleConf.TemplatesHeaderIconSize);
-			playerheaderimage.Pixbuf = Helpers.Misc.LoadIcon ("lm-player-header", StyleConf.TemplatesHeaderIconSize);
-			vseparatorimage.Pixbuf = Helpers.Misc.LoadIcon ("lm-vertical-separator", StyleConf.TemplatesIconSize);
+			teamimage.Image = App.Current.ResourcesLocator.LoadIcon ("lm-team-header", StyleConf.TemplatesHeaderIconSize);
+			playerheaderimage.Image = App.Current.ResourcesLocator.LoadIcon ("lm-player-header", StyleConf.TemplatesHeaderIconSize);
+			// FIXME: "vertical-separator" has a png extension
+			vseparatorimage.Image = new Image (Helpers.Misc.LoadIcon ("lm-vertical-separator", StyleConf.TemplatesIconSize));
+
 
 			newteambutton.Entered += HandleEnterTeamButton;
 			newteambutton.Left += HandleLeftTeamButton;
@@ -82,7 +85,7 @@ namespace LongoMatch.Gui.Panel
 			cell.Edited += HandleEdited;
 			teamseditortreeview.Model = teamsStore;
 			teamseditortreeview.HeadersVisible = false;
-			teamseditortreeview.AppendColumn ("Icon", new CellRendererPixbuf (), RenderIcon);
+			teamseditortreeview.AppendColumn ("Icon", new CellRendererImage (), RenderIcon);
 			var col = teamseditortreeview.AppendColumn ("Text", cell, RenderTemplateName);
 			col.AddAttribute (cell, "editable", COL_EDITABLE);
 			teamseditortreeview.SearchColumn = COL_TEAM;
@@ -185,7 +188,10 @@ namespace LongoMatch.Gui.Panel
 		void RenderIcon (TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter)
 		{
 			LMTeamVM teamVM = (LMTeamVM)model.GetValue (iter, COL_TEAM);
-			(cell as CellRendererPixbuf).Pixbuf = teamVM.Icon.Scale (SHIELD_SIZE, SHIELD_SIZE).Value;
+			CellRendererImage renderer = (cell as CellRendererImage);
+			renderer.Width = SHIELD_SIZE;
+			renderer.Height = SHIELD_SIZE;
+			renderer.Image = teamVM.Icon;
 		}
 
 		void RenderTemplateName (TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter)