hclarosh/longomatch-core

Moved StyleConf values to its own classes under Resources namespace

LongoMatch.Drawing/CanvasObjects/Dashboard/ScoreView.cs
@@ -20,6 +20,7 @@ using VAS.Core;
 using VAS.Core.Common;
 using VAS.Core.Interfaces.MVVMC;
 using VAS.Core.MVVMC;
+using VAS.Core.Resources;
 using VAS.Drawing.CanvasObjects.Dashboard;
 
 namespace LongoMatch.Drawing.CanvasObjects.Dashboard
@@ -31,7 +32,7 @@ namespace LongoMatch.Drawing.CanvasObjects.Dashboard
 
 		static ScoreView ()
 		{
-			iconImage = App.Current.ResourcesLocator.LoadImage (StyleConf.ButtonScoreIcon);
+			iconImage = App.Current.ResourcesLocator.LoadImage (Images.ButtonScoreIcon);
 		}
 
 		public ScoreButtonVM ViewModel {

LongoMatch/longomatch

LongoMatch.Drawing/CanvasObjects/Dashboard/ScoreView.cs
@@ -20,6 +20,7 @@ using VAS.Core;
 using VAS.Core.Common;
 using VAS.Core.Interfaces.MVVMC;
 using VAS.Core.MVVMC;
+using VAS.Core.Resources;
 using VAS.Drawing.CanvasObjects.Dashboard;
 
 namespace LongoMatch.Drawing.CanvasObjects.Dashboard
@@ -31,7 +32,7 @@ namespace LongoMatch.Drawing.CanvasObjects.Dashboard
 
 		static ScoreView ()
 		{
-			iconImage = App.Current.ResourcesLocator.LoadImage (StyleConf.ButtonScoreIcon);
+			iconImage = App.Current.ResourcesLocator.LoadImage (Images.ButtonScore);
 		}
 
 		public ScoreButtonVM ViewModel {