extraymond/longomatch-core

Migrate events about TimelineEvent to VM

LongoMatch.GUI/Gui/Menu/SportsPlaysMenu.cs
@@ -24,30 +24,31 @@ using VAS;
 using VAS.Core;
 using VAS.Core.Events;
 using VAS.Core.Store;
+using VAS.Core.ViewModel;
 using VAS.UI.Menus;
 
 namespace LongoMatch.Gui.Menus
 {
 	public class SportsPlaysMenu : PlaysMenu
 	{
-		protected new void ShowMenu (Project project, IEnumerable<TimelineEvent> plays, EventType eventType, Time time,
+		protected new void ShowMenu (Project project, IEnumerable<TimelineEventVM> plays, EventType eventType, Time time,
 										  IList<EventType> eventTypes, bool editableName)
 		{
 			PrepareMenu (project, plays, eventType, time, eventTypes, editableName);
 			Popup ();
 		}
 
-		public override void ShowMenu (Project project, List<TimelineEvent> plays)
+		public override void ShowMenu (Project project, IEnumerable<TimelineEventVM> playVMs)
 		{
-			ShowMenu (project, plays, null, null, project.EventTypes, true);
+			ShowMenu (project, playVMs, null, null, project.EventTypes, true);
 		}
 
-		protected override void PrepareMenu (Project project, IEnumerable<TimelineEvent> plays, EventType eventType, Time time,
+		protected override void PrepareMenu (Project project, IEnumerable<TimelineEventVM> playVMs, EventType eventType, Time time,
 										 IList<EventType> eventTypes, bool editableName)
 		{
 			bool isLineup = false, isSubstitution = false;
 
-			this.plays = plays.ToList ();
+			this.playVMs = playVMs.ToList ();
 			this.eventType = eventType;
 			this.time = time;
 			this.project = project;
@@ -60,14 +61,14 @@ namespace LongoMatch.Gui.Menus
 				newPlay.Visible = false;
 			}
 
-			if (plays == null) {
-				plays = new List<TimelineEvent> ();
-			} else if (plays.Count () == 1) {
-				isLineup = plays.FirstOrDefault () is LineupEvent;
-				isSubstitution = plays.FirstOrDefault () is SubstitutionEvent;
+			if (playVMs == null) {
+				playVMs = new List<TimelineEventVM> ();
+			} else if (playVMs.Count () == 1) {
+				isLineup = playVMs.FirstOrDefault ().Model is LineupEvent;
+				isSubstitution = playVMs.FirstOrDefault ().Model is SubstitutionEvent;
 			}
 
-			MenuHelpers.FillExportToVideoFileMenu (render, project, plays, Catalog.GetString ("Export to video file"));
+			MenuHelpers.FillExportToVideoFileMenu (render, project, playVMs, Catalog.GetString ("Export to video file"));
 
 			if (isLineup || isSubstitution) {
 				edit.Visible = true;
@@ -75,30 +76,30 @@ namespace LongoMatch.Gui.Menus
 				snapshot.Visible = moveCat.Visible = drawings.Visible =
 					addPLN.Visible = render.Visible = duplicate.Visible = false;
 			} else {
-				edit.Visible = editableName && this.plays.Count == 1;
-				snapshot.Visible = this.plays.Count == 1;
-				drawings.Visible = this.plays.Count == 1 && this.plays.FirstOrDefault ().Drawings.Count > 0;
-				moveCat.Visible = del.Visible = addPLN.Visible = duplicate.Visible = this.plays.Any ();
+				edit.Visible = editableName && this.playVMs.Count == 1;
+				snapshot.Visible = this.playVMs.Count == 1;
+				drawings.Visible = this.playVMs.Count == 1 && this.playVMs.FirstOrDefault ().Drawings.Count > 0;
+				moveCat.Visible = del.Visible = addPLN.Visible = duplicate.Visible = this.playVMs.Any ();
 			}
 
-			if (plays.Count () > 0) {
-				string label = String.Format ("{0} ({1})", Catalog.GetString ("Delete"), plays.Count ());
+			if (playVMs.Count () > 0) {
+				string label = String.Format ("{0} ({1})", Catalog.GetString ("Delete"), playVMs.Count ());
 				del.SetLabel (label);
-				label = String.Format ("{0} ({1})", Catalog.GetString ("Duplicate "), plays.Count ());
+				label = String.Format ("{0} ({1})", Catalog.GetString ("Duplicate "), playVMs.Count ());
 				duplicate.SetLabel (label);
 			}
 
 			if (moveCat.Visible) {
 				Menu catMenu = new Menu ();
 				foreach (EventType c in eventTypes) {
-					if (plays.Any (p => p.EventType == c))
+					if (playVMs.Any (p => p.EventType == c))
 						continue;
 					var item = new MenuItem (c.Name);
 					catMenu.Append (item);
 					item.Activated += (sender, e) => {
 						App.Current.EventsBroker.Publish<MoveToEventTypeEvent> (
 							new MoveToEventTypeEvent {
-								TimelineEvents = plays.ToList (),
+								TimelineEventVMs = playVMs.ToList (),
 								EventType = c
 							}
 						);
@@ -110,7 +111,7 @@ namespace LongoMatch.Gui.Menus
 
 			if (drawings.Visible) {
 				Menu drawingsMenu = new Menu ();
-				for (int i = 0; i < plays.FirstOrDefault ().Drawings.Count; i++) {
+				for (int i = 0; i < playVMs.FirstOrDefault ().Drawings.Count; i++) {
 					int index = i;
 					MenuItem drawingItem = new MenuItem (Catalog.GetString ("Drawing ") + (i + 1));
 					MenuItem editItem = new MenuItem (Catalog.GetString ("Edit"));
@@ -121,18 +122,18 @@ namespace LongoMatch.Gui.Menus
 					drawingMenu.Append (editItem);
 					drawingMenu.Append (deleteItem);
 					editItem.Activated += (sender, e) => {
-						var play = plays.FirstOrDefault ();
+						var playVM = playVMs.FirstOrDefault ();
 						App.Current.EventsBroker.Publish (
 							new DrawFrameEvent {
-								Play = play,
+								Play = playVM,
 								DrawingIndex = index,
-								CamConfig = play.Drawings [index].CameraConfig,
+								CamConfig = playVM.Drawings [index].CameraConfig,
 							}
 						);
 					};
 					deleteItem.Activated += (sender, e) => {
-						plays.FirstOrDefault ().Drawings.RemoveAt (index);
-						plays.FirstOrDefault ().UpdateMiniature ();
+						playVMs.FirstOrDefault ().Drawings.RemoveAt (index);
+						playVMs.FirstOrDefault ().Model.UpdateMiniature ();
 					};
 					drawingItem.Submenu = drawingMenu;
 					drawingMenu.ShowAll ();
@@ -142,7 +143,8 @@ namespace LongoMatch.Gui.Menus
 			}
 
 			if (!IsLineupEvent ()) {
-				MenuHelpers.FillAddToPlaylistMenu (addPLN, project.Playlists, this.plays);
+				var playlistVMs = project.Playlists.Select (pl => new PlaylistVM { Model = pl });
+				MenuHelpers.FillAddToPlaylistMenu (addPLN, playlistVMs, this.playVMs);
 			}
 		}
 
@@ -154,17 +156,17 @@ namespace LongoMatch.Gui.Menus
 			edit.Activated += (sender, e) => {
 				App.Current.EventsBroker.Publish (
 				new EditEventEvent {
-					TimelineEvent = plays.Single ()
+					TimelineEvent = playVMs.Single ()
 				});
 			};
 			Add (edit);
 
 			duplicate = new MenuItem ("");
 			duplicate.Activated += (sender, e) => App.Current.EventsBroker.Publish<DuplicateEventsEvent> (
-					new DuplicateEventsEvent {
-						TimelineEvents = plays
-					}
-				);
+				new DuplicateEventsEvent {
+					TimelineEvents = playVMs
+				}
+			);
 			Add (duplicate);
 
 			moveCat = new MenuItem (Catalog.GetString ("Move to"));
@@ -177,13 +179,13 @@ namespace LongoMatch.Gui.Menus
 			Add (addPLN);
 
 			render = new MenuItem ("");
-			render.Activated += (sender, e) => MenuHelpers.EmitRenderPlaylist (plays);
+			render.Activated += (sender, e) => MenuHelpers.EmitRenderPlaylist (playVMs);
 			Add (render);
 
 			snapshot = new MenuItem (Catalog.GetString ("Export to PNG images"));
 			snapshot.Activated += (sender, e) => App.Current.EventsBroker.Publish<SnapshotSeriesEvent> (
 				new SnapshotSeriesEvent {
-					TimelineEvent = plays.FirstOrDefault ()
+					TimelineEvent = playVMs.FirstOrDefault ()
 				}
 			);
 			Add (snapshot);
@@ -193,7 +195,7 @@ namespace LongoMatch.Gui.Menus
 
 		bool IsLineupEvent ()
 		{
-			return plays.Any (p => p is LineupEvent || p is SubstitutionEvent);
+			return playVMs.Any (p => p.Model is LineupEvent || p.Model is SubstitutionEvent);
 		}
 	}
 }

LongoMatch.GUI/Gui/TreeView/LMTimelineEventsTreeView.cs
@@ -147,14 +147,16 @@ namespace LongoMatch.Gui.Component
 		protected override void ShowMenu ()
 		{
 			IEnumerable<IViewModel> viewModels = GetSelectedViewModels ();
-			IEnumerable<TimelineEventVM> events = viewModels.OfType<TimelineEventVM> ();
+			IEnumerable<TimelineEventVM> eventVMs = viewModels.OfType<TimelineEventVM> ();
 
 			EventTypeTimelineVM categoryVM = viewModels.OfType<EventTypeTimelineVM> ().FirstOrDefault ();
-			if (!events.Any () && categoryVM != null) {
-				events = categoryVM.ViewModels.Where (vm => vm.Visible);
-				eventTypeMenu.ShowMenu (Project.Model, categoryVM.Model, events.Select (vm => vm.Model as LMTimelineEvent).ToList ());
+
+			if (!eventVMs.Any () && categoryVM != null) {
+				eventVMs = categoryVM.ViewModels.Where (vm => vm.Visible);
+				eventTypeMenu.ShowMenu (Project.Model, categoryVM.Model,
+										eventVMs.Select (vm => vm as LMTimelineEventVM));
 			} else {
-				menu.ShowMenu (Project.Model, events.Select (vm => vm.Model).ToList ());
+				menu.ShowMenu (Project.Model, eventVMs);
 			}
 		}
 

LongoMatch.Services/Controller/LMEventsController.cs
@@ -34,6 +34,7 @@ using VAS.Core.Hotkeys;
 using VAS.Core.Interfaces.MVVMC;
 using VAS.Core.MVVMC;
 using VAS.Core.Store;
+using VAS.Core.ViewModel;
 using VAS.Services.Controller;
 using KeyAction = VAS.Core.Hotkeys.KeyAction;
 
@@ -50,14 +51,14 @@ namespace LongoMatch.Services
 		public override async Task Start ()
 		{
 			await base.Start ();
-			App.Current.EventsBroker.Subscribe<LoadTimelineEventEvent<TimelineEvent>> (HandleLoadTimelineEvent);
+			App.Current.EventsBroker.Subscribe<LoadTimelineEventEvent<TimelineEventVM>> (HandleLoadTimelineEvent);
 			App.Current.EventsBroker.Subscribe<PlayerSubstitutionEvent> (HandlePlayerSubstitutionEvent);
 			App.Current.EventsBroker.Subscribe<ShowProjectStatsEvent> (HandleShowProjectStatsEvent);
 		}
 
 		public override async Task Stop ()
 		{
-			App.Current.EventsBroker.Unsubscribe<LoadTimelineEventEvent<TimelineEvent>> (HandleLoadTimelineEvent);
+			App.Current.EventsBroker.Unsubscribe<LoadTimelineEventEvent<TimelineEventVM>> (HandleLoadTimelineEvent);
 			App.Current.EventsBroker.Unsubscribe<PlayerSubstitutionEvent> (HandlePlayerSubstitutionEvent);
 			App.Current.EventsBroker.Unsubscribe<ShowProjectStatsEvent> (HandleShowProjectStatsEvent);
 			await base.Stop ();
@@ -78,7 +79,7 @@ namespace LongoMatch.Services
 		}
 
 		// FIXME: remove this when the video capturer is ported to MVVM
-		void HandleLoadTimelineEvent (LoadTimelineEventEvent<TimelineEvent> e)
+		void HandleLoadTimelineEvent (LoadTimelineEventEvent<TimelineEventVM> e)
 		{
 			VideoPlayer.LoadEvent (e.Object, e.Playing);
 		}
@@ -94,7 +95,7 @@ namespace LongoMatch.Services
 				evt = viewModel.Project.Model.SubsitutePlayer (e.Team, e.Player1, e.Player2, e.SubstitutionReason, e.Time);
 				App.Current.EventsBroker.Publish (
 					new EventCreatedEvent {
-						TimelineEvent = evt
+						TimelineEvent = new TimelineEventVM () { Model = evt }
 					}
 				);
 			} catch (SubstitutionException ex) {
@@ -109,25 +110,25 @@ namespace LongoMatch.Services
 
 		void DeleteLoadedEvent ()
 		{
-			if (LoadedPlay == null) {
+			if (LoadedPlayVM?.Model == null) {
 				return;
 			}
 			App.Current.EventsBroker.Publish (
 				new EventsDeletedEvent {
-					TimelineEvents = new List<TimelineEvent> { LoadedPlay }
+					TimelineEventVMs = new List<TimelineEventVM> { LoadedPlayVM }
 				}
 			);
 		}
 
 		void EditLoadedEvent ()
 		{
-			if (LoadedPlay == null) {
+			if (LoadedPlayVM?.Model == null) {
 				return;
 			}
 			bool playing = VideoPlayer.Playing;
 			VideoPlayer.PauseCommand.Execute (false);
 
-			App.Current.EventsBroker.Publish (new EditEventEvent { TimelineEvent = LoadedPlay });
+			App.Current.EventsBroker.Publish (new EditEventEvent { TimelineEvent = LoadedPlayVM });
 
 			if (playing) {
 				VideoPlayer.PlayCommand.Execute ();

LongoMatch.Services/Controller/LMTeamTaggerController.cs
@@ -397,14 +397,14 @@ namespace LongoMatch.Services.Controller
 
 		void HandleEventsDeletedEvent (EventsDeletedEvent e)
 		{
-			if (e.TimelineEvents.Count (s => s is SubstitutionEvent) != 0) {
+			if (e.TimelineEventVMs.Count (s => s.Model is SubstitutionEvent) != 0) {
 				UpdatePlayersPosition ();
 			}
 		}
 
 		void HandleEventEdited (EventEditedEvent e)
 		{
-			if (e.TimelineEvent is SubstitutionEvent || e.TimelineEvent is LineupEvent) {
+			if (e.TimelineEventVM.Model is SubstitutionEvent || e.TimelineEventVM.Model is LineupEvent) {
 				UpdatePlayersPosition ();
 			}
 		}

LongoMatch.Services/Controller/PlayEditorController.cs
@@ -42,7 +42,7 @@ namespace LongoMatch.Services.Controller
 		{
 			if (teamTagger.NeedsSync (e.PropertyName, "Collection_Selection", sender, teamTagger.HomeTeam) ||
 				teamTagger.NeedsSync (e.PropertyName, "Collection_Selection", sender, teamTagger.AwayTeam)) {
-				playEditorVM.Play.Players.Reset (teamTagger.HomeTeam.Selection.Select (p => p.Model)
+				playEditorVM.PlayVM.Players.Reset (teamTagger.HomeTeam.Selection.Select (p => p.Model)
 												   .Concat (teamTagger.AwayTeam.Selection.Select (p => p.Model)));
 			}
 
@@ -57,12 +57,10 @@ namespace LongoMatch.Services.Controller
 
 		void UpdatePlayTeams (LMTeamVM team)
 		{
-			//FIXME: this is using playEditorVM.Play as Model, it should use TimelineEventVM,
-			//when the PlayEditor view is fully migrated to MVVM
-			if (team.Tagged && !playEditorVM.Play.Teams.Contains (team.Model)) {
-				playEditorVM.Play.Teams.Add (team.Model);
-			} else if (!team.Tagged && playEditorVM.Play.Teams.Contains (team.Model)) {
-				playEditorVM.Play.Teams.Remove (team.Model);
+			if (team.Tagged && !playEditorVM.PlayVM.Teams.Contains (team.Model)) {
+				playEditorVM.PlayVM.Teams.Add (team.Model);
+			} else if (!team.Tagged && playEditorVM.PlayVM.Teams.Contains (team.Model)) {
+				playEditorVM.PlayVM.Teams.Remove (team.Model);
 			}
 		}
 	}

Tests/Controller/TestLMEventsController.cs
@@ -93,15 +93,16 @@ namespace Tests.Controller
 			var ev1 = new TimelineEvent ();
 			ev1.Start = new Time (0);
 			ev1.Stop = new Time (1000);
+			var vm1 = new TimelineEventVM () { Model = ev1 };
 
 			App.Current.EventsBroker.Publish (
-					new LoadTimelineEventEvent<TimelineEvent> {
-						Object = ev1,
-						Playing = true,
-					}
-				);
+				new LoadTimelineEventEvent<TimelineEventVM> {
+					Object = vm1,
+					Playing = true,
+				}
+			);
 
-			playerController.Verify (p => p.LoadEvent (ev1, ev1.Start, true));
+			playerController.Verify (p => p.LoadEvent (vm1, ev1.Start, true));
 		}
 
 		[Test]
@@ -143,14 +144,16 @@ namespace Tests.Controller
 
 			Assert.AreEqual (currentCount, projectVM.Timeline.FullTimeline.Count ());
 			mockLimitationService.Verify (ls => ls.MoveToUpgradeDialog (VASCountLimitedObjects.TimelineEvents.ToString ()),
-			                              Times.Once);
+										  Times.Once);
 		}
 
 		[Test]
 		public void EventsDeletedEvent_DeleteAllEvents_LineupEventNotDeleted ()
 		{
+			var eventVMs = projectVM.Timeline.Model.Select (e => new TimelineEventVM () { Model = e });
+
 			App.Current.EventsBroker.Publish (new EventsDeletedEvent {
-				TimelineEvents = projectVM.Timeline.Model
+				TimelineEventVMs = eventVMs
 			});
 
 			Assert.AreEqual (1, projectVM.Timeline.Model.Count);

Tests/Integration/TestPostGameAnalysis.cs
@@ -195,12 +195,14 @@ namespace Tests.Integration
 			Assert.AreEqual (5, savedP.Timeline.Count);
 
 			// Delete some events
-			await App.Current.EventsBroker.Publish<EventsDeletedEvent> (
-				new EventsDeletedEvent {
-					TimelineEvents = new List<TimelineEvent> {
+			var events = new List<TimelineEvent> {
 						p.Timeline [0],
 						p.Timeline [1]
-					}
+			};
+
+			await App.Current.EventsBroker.Publish<EventsDeletedEvent> (
+				new EventsDeletedEvent {
+					TimelineEventVMs = events.Select (e => new TimelineEventVM () { Model = e })
 				}
 			);
 			Assert.AreEqual (3, p.Timeline.Count);

LongoMatch/longomatch

LongoMatch.GUI/Gui/Menu/SportsPlaysMenu.cs
@@ -24,25 +24,26 @@ using VAS;
 using VAS.Core;
 using VAS.Core.Events;
 using VAS.Core.Store;
+using VAS.Core.ViewModel;
 using VAS.UI.Menus;
 
 namespace LongoMatch.Gui.Menus
 {
 	public class SportsPlaysMenu : PlaysMenu
 	{
-		protected new void ShowMenu (Project project, IEnumerable<TimelineEvent> plays, EventType eventType, Time time,
+		protected new void ShowMenu (Project project, IEnumerable<TimelineEventVM> plays, EventType eventType, Time time,
 										  IList<EventType> eventTypes, bool editableName)
 		{
 			PrepareMenu (project, plays, eventType, time, eventTypes, editableName);
 			Popup ();
 		}
 
-		public override void ShowMenu (Project project, List<TimelineEvent> plays)
+		public override void ShowMenu (Project project, IEnumerable<TimelineEventVM> plays)
 		{
 			ShowMenu (project, plays, null, null, project.EventTypes, true);
 		}
 
-		protected override void PrepareMenu (Project project, IEnumerable<TimelineEvent> plays, EventType eventType, Time time,
+		protected override void PrepareMenu (Project project, IEnumerable<TimelineEventVM> plays, EventType eventType, Time time,
 										 IList<EventType> eventTypes, bool editableName)
 		{
 			bool isLineup = false, isSubstitution = false;
@@ -61,10 +62,10 @@ namespace LongoMatch.Gui.Menus
 			}
 
 			if (plays == null) {
-				plays = new List<TimelineEvent> ();
+				plays = new List<TimelineEventVM> ();
 			} else if (plays.Count () == 1) {
-				isLineup = plays.FirstOrDefault () is LineupEvent;
-				isSubstitution = plays.FirstOrDefault () is SubstitutionEvent;
+				isLineup = plays.FirstOrDefault ().Model is LineupEvent;
+				isSubstitution = plays.FirstOrDefault ().Model is SubstitutionEvent;
 			}
 
 			MenuHelpers.FillExportToVideoFileMenu (render, project, plays, Catalog.GetString ("Export to video file"));
@@ -132,7 +133,7 @@ namespace LongoMatch.Gui.Menus
 					};
 					deleteItem.Activated += (sender, e) => {
 						plays.FirstOrDefault ().Drawings.RemoveAt (index);
-						plays.FirstOrDefault ().UpdateMiniature ();
+						plays.FirstOrDefault ().Model.UpdateMiniature ();
 					};
 					drawingItem.Submenu = drawingMenu;
 					drawingMenu.ShowAll ();
@@ -142,7 +143,8 @@ namespace LongoMatch.Gui.Menus
 			}
 
 			if (!IsLineupEvent ()) {
-				MenuHelpers.FillAddToPlaylistMenu (addPLN, project.Playlists, this.plays);
+				var playlistVMs = project.Playlists.Select (pl => new PlaylistVM { Model = pl });
+				MenuHelpers.FillAddToPlaylistMenu (addPLN, playlistVMs, this.plays);
 			}
 		}
 
@@ -161,10 +163,10 @@ namespace LongoMatch.Gui.Menus
 
 			duplicate = new MenuItem ("");
 			duplicate.Activated += (sender, e) => App.Current.EventsBroker.Publish<DuplicateEventsEvent> (
-					new DuplicateEventsEvent {
-						TimelineEvents = plays
-					}
-				);
+				new DuplicateEventsEvent {
+					TimelineEvents = plays
+				}
+			);
 			Add (duplicate);
 
 			moveCat = new MenuItem (Catalog.GetString ("Move to"));
@@ -193,7 +195,7 @@ namespace LongoMatch.Gui.Menus
 
 		bool IsLineupEvent ()
 		{
-			return plays.Any (p => p is LineupEvent || p is SubstitutionEvent);
+			return plays.Any (p => p.Model is LineupEvent || p.Model is SubstitutionEvent);
 		}
 	}
 }

LongoMatch.GUI/Gui/TreeView/LMTimelineEventsTreeView.cs
@@ -150,11 +150,13 @@ namespace LongoMatch.Gui.Component
 			IEnumerable<TimelineEventVM> events = viewModels.OfType<TimelineEventVM> ();
 
 			EventTypeTimelineVM categoryVM = viewModels.OfType<EventTypeTimelineVM> ().FirstOrDefault ();
+
 			if (!events.Any () && categoryVM != null) {
 				events = categoryVM.ViewModels.Where (vm => vm.Visible);
-				eventTypeMenu.ShowMenu (Project.Model, categoryVM.Model, events.Select (vm => vm.Model as LMTimelineEvent).ToList ());
+				eventTypeMenu.ShowMenu (Project.Model, categoryVM.Model,
+										events.Select (vm => vm as LMTimelineEventVM));
 			} else {
-				menu.ShowMenu (Project.Model, events.Select (vm => vm.Model).ToList ());
+				menu.ShowMenu (Project.Model, events);
 			}
 		}
 

LongoMatch.Services/Controller/LMEventsController.cs
@@ -22,7 +22,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LongoMatch.Core.Common;
-//using LongoMatch.Core.Common;
 using LongoMatch.Core.Events;
 using LongoMatch.Core.Hotkeys;
 using LongoMatch.Core.Store;
@@ -34,8 +33,10 @@ using VAS.Core.Hotkeys;
 using VAS.Core.Interfaces.MVVMC;
 using VAS.Core.MVVMC;
 using VAS.Core.Store;
+using VAS.Core.ViewModel;
 using VAS.Services.Controller;
 using KeyAction = VAS.Core.Hotkeys.KeyAction;
+using System.Linq;
 
 namespace LongoMatch.Services
 {
@@ -50,14 +51,14 @@ namespace LongoMatch.Services
 		public override async Task Start ()
 		{
 			await base.Start ();
-			App.Current.EventsBroker.Subscribe<LoadTimelineEventEvent<TimelineEvent>> (HandleLoadTimelineEvent);
+			App.Current.EventsBroker.Subscribe<LoadTimelineEventEvent<TimelineEventVM>> (HandleLoadTimelineEvent);
 			App.Current.EventsBroker.Subscribe<PlayerSubstitutionEvent> (HandlePlayerSubstitutionEvent);
 			App.Current.EventsBroker.Subscribe<ShowProjectStatsEvent> (HandleShowProjectStatsEvent);
 		}
 
 		public override async Task Stop ()
 		{
-			App.Current.EventsBroker.Unsubscribe<LoadTimelineEventEvent<TimelineEvent>> (HandleLoadTimelineEvent);
+			App.Current.EventsBroker.Unsubscribe<LoadTimelineEventEvent<TimelineEventVM>> (HandleLoadTimelineEvent);
 			App.Current.EventsBroker.Unsubscribe<PlayerSubstitutionEvent> (HandlePlayerSubstitutionEvent);
 			App.Current.EventsBroker.Unsubscribe<ShowProjectStatsEvent> (HandleShowProjectStatsEvent);
 			await base.Stop ();
@@ -78,7 +79,7 @@ namespace LongoMatch.Services
 		}
 
 		// FIXME: remove this when the video capturer is ported to MVVM
-		void HandleLoadTimelineEvent (LoadTimelineEventEvent<TimelineEvent> e)
+		void HandleLoadTimelineEvent (LoadTimelineEventEvent<TimelineEventVM> e)
 		{
 			VideoPlayer.LoadEvent (e.Object, e.Playing);
 		}
@@ -92,9 +93,12 @@ namespace LongoMatch.Services
 
 			try {
 				evt = viewModel.Project.Model.SubsitutePlayer (e.Team, e.Player1, e.Player2, e.SubstitutionReason, e.Time);
+
+				var timelineEventVM = viewModel.Project.Timeline.FullTimeline.Where (x => x.Model == evt).FirstOrDefault ();
+
 				App.Current.EventsBroker.Publish (
 					new EventCreatedEvent {
-						TimelineEvent = evt
+						TimelineEvent = timelineEventVM
 					}
 				);
 			} catch (SubstitutionException ex) {
@@ -109,19 +113,19 @@ namespace LongoMatch.Services
 
 		void DeleteLoadedEvent ()
 		{
-			if (LoadedPlay == null) {
+			if (LoadedPlay?.Model == null) {
 				return;
 			}
 			App.Current.EventsBroker.Publish (
 				new EventsDeletedEvent {
-					TimelineEvents = new List<TimelineEvent> { LoadedPlay }
+					TimelineEvents = new List<TimelineEventVM> { LoadedPlay }
 				}
 			);
 		}
 
 		void EditLoadedEvent ()
 		{
-			if (LoadedPlay == null) {
+			if (LoadedPlay?.Model == null) {
 				return;
 			}
 			bool playing = VideoPlayer.Playing;

LongoMatch.Services/Controller/LMTeamTaggerController.cs
@@ -397,14 +397,14 @@ namespace LongoMatch.Services.Controller
 
 		void HandleEventsDeletedEvent (EventsDeletedEvent e)
 		{
-			if (e.TimelineEvents.Count (s => s is SubstitutionEvent) != 0) {
+			if (e.TimelineEvents.Count (s => s.Model is SubstitutionEvent) != 0) {
 				UpdatePlayersPosition ();
 			}
 		}
 
 		void HandleEventEdited (EventEditedEvent e)
 		{
-			if (e.TimelineEvent is SubstitutionEvent || e.TimelineEvent is LineupEvent) {
+			if (e.TimelineEvent.Model is SubstitutionEvent || e.TimelineEvent.Model is LineupEvent) {
 				UpdatePlayersPosition ();
 			}
 		}

LongoMatch.Services/Controller/PlayEditorController.cs
@@ -17,13 +17,13 @@ namespace LongoMatch.Services.Controller
 	[Controller (PlayEditorState.NAME)]
 	public class PlayEditorController : ControllerBase
 	{
-		PlayEditorVM playEditorVM;
+		PlayEditorVM playEditor;
 		LMTeamTaggerVM teamTagger;
 
 		public override void SetViewModel (IViewModel viewModel)
 		{
-			playEditorVM = (PlayEditorVM)viewModel;
-			teamTagger = playEditorVM.TeamTagger;
+			playEditor = (PlayEditorVM)viewModel;
+			teamTagger = playEditor.TeamTagger;
 		}
 
 		public override async Task Start ()
@@ -42,7 +42,7 @@ namespace LongoMatch.Services.Controller
 		{
 			if (teamTagger.NeedsSync (e.PropertyName, "Collection_Selection", sender, teamTagger.HomeTeam) ||
 				teamTagger.NeedsSync (e.PropertyName, "Collection_Selection", sender, teamTagger.AwayTeam)) {
-				playEditorVM.Play.Players.Reset (teamTagger.HomeTeam.Selection.Select (p => p.Model)
+				playEditor.Play.Players.Reset (teamTagger.HomeTeam.Selection.Select (p => p.Model)
 												   .Concat (teamTagger.AwayTeam.Selection.Select (p => p.Model)));
 			}
 
@@ -57,12 +57,10 @@ namespace LongoMatch.Services.Controller
 
 		void UpdatePlayTeams (LMTeamVM team)
 		{
-			//FIXME: this is using playEditorVM.Play as Model, it should use TimelineEventVM,
-			//when the PlayEditor view is fully migrated to MVVM
-			if (team.Tagged && !playEditorVM.Play.Teams.Contains (team.Model)) {
-				playEditorVM.Play.Teams.Add (team.Model);
-			} else if (!team.Tagged && playEditorVM.Play.Teams.Contains (team.Model)) {
-				playEditorVM.Play.Teams.Remove (team.Model);
+			if (team.Tagged && !playEditor.Play.Teams.Contains (team.Model)) {
+				playEditor.Play.Teams.Add (team.Model);
+			} else if (!team.Tagged && playEditor.Play.Teams.Contains (team.Model)) {
+				playEditor.Play.Teams.Remove (team.Model);
 			}
 		}
 	}

Tests/Controller/TestLMEventsController.cs
@@ -99,15 +99,16 @@ namespace Tests.Controller
 			var ev1 = new TimelineEvent ();
 			ev1.Start = new Time (0);
 			ev1.Stop = new Time (1000);
+			var vm1 = new TimelineEventVM () { Model = ev1 };
 
 			App.Current.EventsBroker.Publish (
-					new LoadTimelineEventEvent<TimelineEvent> {
-						Object = ev1,
-						Playing = true,
-					}
-				);
+				new LoadTimelineEventEvent<TimelineEventVM> {
+					Object = vm1,
+					Playing = true,
+				}
+			);
 
-			playerController.Verify (p => p.LoadEvent (ev1, ev1.Start, true));
+			playerController.Verify (p => p.LoadEvent (vm1, ev1.Start, true));
 		}
 
 		[Test]
@@ -149,14 +150,16 @@ namespace Tests.Controller
 
 			Assert.AreEqual (currentCount, projectVM.Timeline.FullTimeline.Count ());
 			mockLimitationService.Verify (ls => ls.MoveToUpgradeDialog (VASCountLimitedObjects.TimelineEvents.ToString ()),
-			                              Times.Once);
+										  Times.Once);
 		}
 
 		[Test]
 		public void EventsDeletedEvent_DeleteAllEvents_LineupEventNotDeleted ()
 		{
+			var eventVMs = projectVM.Timeline.Model.Select (e => new TimelineEventVM () { Model = e });
+
 			App.Current.EventsBroker.Publish (new EventsDeletedEvent {
-				TimelineEvents = projectVM.Timeline.Model
+				TimelineEvents = eventVMs
 			});
 
 			Assert.AreEqual (1, projectVM.Timeline.Model.Count);

Tests/Integration/TestPostGameAnalysis.cs
@@ -197,12 +197,14 @@ namespace Tests.Integration
 			Assert.AreEqual (5, savedP.Timeline.Count);
 
 			// Delete some events
-			await App.Current.EventsBroker.Publish<EventsDeletedEvent> (
-				new EventsDeletedEvent {
-					TimelineEvents = new List<TimelineEvent> {
+			var events = new List<TimelineEvent> {
 						p.Timeline [0],
 						p.Timeline [1]
-					}
+			};
+
+			await App.Current.EventsBroker.Publish<EventsDeletedEvent> (
+				new EventsDeletedEvent {
+					TimelineEvents = events.Select (e => new TimelineEventVM () { Model = e })
 				}
 			);
 			Assert.AreEqual (3, p.Timeline.Count);