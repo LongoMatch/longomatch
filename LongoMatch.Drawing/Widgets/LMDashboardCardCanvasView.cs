137fee737a2407654176b1929fd132a7152ddcbe
Dani Garcia Sanchez <dgarcia@fluendo.com>
2018-02-05 19:09:00 +0100

    Add Dashboard Manager Page

---
+++ LongoMatch.Drawing/Widgets/LMDashboardCardCanvasView.cs
@@ -0,0 +1,36 @@
+﻿//
+//  Copyright (C) 2018 Fluendo S.A.
+//
+//
+using System;
+using LongoMatch.Core.Resources;
+using LongoMatch.Core.ViewModel;
+using VAS.Core.Common;
+using VAS.Drawing.Widgets;
+
+namespace LongoMatch.Drawing.Widgets
+{
+	public class LMDashboardCardCanvasView : CardCanvasView<LMDashboardVM>
+	{
+
+		protected override string Title => ViewModel.Name;
+
+		protected override string SubTitle => $"{ViewModel.ViewModels.Count} {Strings.EventButtons}";
+
+		protected override DateTime CreationDate => ViewModel.Model.CreationDate;
+
+		protected override void DrawBackgroundImage ()
+		{
+			if (ViewModel.Preview != null) {
+				tk.DrawImage (cardDetailArea.Start, cardDetailArea.Width, cardDetailArea.Height,
+				ViewModel.Preview, ScaleMode.AspectFit);
+			} else {
+				base.DrawBackgroundImage ();
+			}
+		}
+
+		protected override void DrawContent ()
+		{
+		}
+	}
+}
