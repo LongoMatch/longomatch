8d162227d22dbd84179c0382fe9301f96949d6f7
Dani Garcia Sanchez <dgarcia@fluendo.com>
2018-02-14 17:49:16 +0100

    Add Strings Resources

--- LongoMatch.Core/Resources/Strings.cs
+++ LongoMatch.Core/Resources/Strings.cs
@@ -13,5 +13,10 @@ namespace LongoMatch.Core.Resources
 		public static string TrialExpireInOneDay => Catalog.GetString ("Your trial period will expire in <b>1 day</b>");
 		public static string TrialExpireToday => Catalog.GetString ("Your trial period will expire <b>today</b>");
 		public static string UpgradeToUnlock => Catalog.GetString ("Upgrade to unlock more features!");
+		
+		public static string NoName => Catalog.GetString ("No Name");
+		public static string NoLeague => Catalog.GetString ("No League");
+		public static string NoSeason => Catalog.GetString ("No Season");
+		public static string NoMatchDate => Catalog.GetString ("No Match Date");
 	}
 }
