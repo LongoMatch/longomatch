e97f68cec193435e878f8f6bbf63086f171de08b
Dani Garcia Sanchez <dgarcia@fluendo.com>
2018-02-27 16:32:46 +0100

    LMA-262 Navigate Back error

--- LongoMatch.Services/State/ProjectsManagerState.cs
+++ LongoMatch.Services/State/ProjectsManagerState.cs
@@ -42,6 +42,12 @@ namespace LongoMatch.Services.State
 		protected override void CreateViewModel (dynamic data)
 		{
 			ViewModel = new SportsProjectsManagerVM ();
+			ViewModel.Model.Reset (App.Current.DatabaseManager.ActiveDB.RetrieveAll<LMProject> ().
+								   SortByCreationDate (true));
+			if (ViewModel.Selection.Count == 0) {
+				ViewModel.Select (ViewModel.ViewModels.FirstOrDefault ());
+			}
+
 			if (App.Current.LicenseLimitationsService != null) {
 				ViewModel.LimitationChart = App.Current.LicenseLimitationsService.CreateBarChartVM (
 					VASCountLimitedObjects.Projects.ToString ());
@@ -57,13 +63,6 @@ namespace LongoMatch.Services.State
 		{
 			if (!await base.ShowState ()) {
 				return false;
-			}
-
-			ViewModel.Model.Reset (App.Current.DatabaseManager.ActiveDB.RetrieveAll<LMProject> ().
-								   SortByCreationDate (true));
-
-			if (ViewModel.Selection.Count == 0) {
-				ViewModel.Select (ViewModel.ViewModels.FirstOrDefault ());
 			}
 			return true;
 		}
