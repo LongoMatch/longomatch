7d18ede7de5f26f2ed82d4a6abf87b775347ac97
dfernandez <dfernandez@fluendo.com>
2018-04-24 11:03:54 +0200

    Revert "Fix saving projects after export/resync"
    This reverts commit ef124d8885e3ad5112f605eabc103783919981bd.

--- LongoMatch.Services/Controller/SportsProjectsController.cs
+++ LongoMatch.Services/Controller/SportsProjectsController.cs
@@ -66,9 +66,8 @@ namespace LongoMatch.Services.Controller
 
 			dynamic data = new System.Dynamic.ExpandoObject ();
 
-			LMProjectVM projectVM = ViewModel.ViewModels.FirstOrDefault (vm => vm.Model.ID.Equals (ViewModel.LoadedProject.Model.ID));
-			await projectVM.LoadModel ();
-			data.ProjectVM = projectVM;
+			ViewModel.LoadedProject.Model.Load ();
+			data.ProjectVM = ViewModel.LoadedProject;
 			data.SynchronizeEventsWithPeriods = false;
 			await App.Current.StateController.MoveTo (CameraSynchronizationEditorState.NAME, data);
 			ViewModel.SaveCommand.EmitCanExecuteChanged ();
